0cf47076ac83bbb4e57e76179787d795f65599c9
Mardax <62964422+mardax007@users.noreply.github.com>
2022-12-02 07:03:55 +0100

    fix: :bug: fix ts

--- src/2022/01/part2.ts
+++ src/2022/01/part2.ts
@@ -1,18 +1,21 @@
 export default function solution(input:string) {
-	const cleanInput = input.split("\n\t\n\t")
+	const cleanInput = input.split("\r\n\r\n")
+
+	console.log(cleanInput)
 
 	let topCals: number[] = []
 
 	cleanInput.forEach((elfInv: string) => {
 		let totalCal = 0
-		elfInv.split("\n\t").forEach(cal => {
+		elfInv.split("\r\n").forEach(cal => {
 			totalCal = totalCal + parseInt(cal)
 		});
 
 		topCals.push(totalCal)
 	});
 
-	topCals.sort((a, b) => b - a)
+	
 
+	topCals.sort((a, b) => b - a)
 	return topCals[0] + topCals[1] + topCals[2];
 }
